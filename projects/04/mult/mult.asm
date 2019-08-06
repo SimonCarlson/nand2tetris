629162872ce8fba5e2123e130fdfc6e5599b5adb
simoncarlson <cimon.carlson@gmail.com>
2019-08-06 17:03:39 +0200

    Multiplication implementation

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @R1
+    D=M     // Save value of R1
+    @i
+    M=D     // Save value of R1 to i
+    @R2
+    M=0     // Initialize the result to 0
+
+(LOOP)
+    @i  
+    D=M     // Load i
+    @END
+    D;JEQ   // If i == 0, end loop
+
+    @R0
+    D=M
+    @R2
+    M=M+D   // Add R0 to R2
+
+    @i
+    M=M-1   // Decrement i
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
