a49568da676a97b3844accb303755d5325caf50d
simoncarlson <cimon.carlson@gmail.com>
2019-08-06 17:33:37 +0200

    Screen fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @8192   // 512*256/16 pixels
+    D=A
+    @buffer
+    M=D     // Buffer size
+
+(LOOP)
+    @i
+    M=0     // Loop variable
+
+(INNER)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ   // Go to white if no key is pressed
+
+(BLACK)
+    @i
+    D=M
+    @SCREEN
+    A=A+D   // Calculate address
+    M=-1    // Set it to black
+    @END
+    0;JMP
+
+(WHITE)
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+
+(END)
+    @i
+    MD=M+1
+    @buffer
+    D=D-M
+    @LOOP
+    D;JEQ
+    @INNER
+    0;JMP
+
